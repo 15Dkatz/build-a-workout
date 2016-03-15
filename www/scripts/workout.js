63793d64672bee33fe8b7149d271167622d74c30
davidkatz <15Dkatz@shcp.edu>
2016-03-15 01:02:46 -0700

    Must fix workout.

--- www/scripts/workout.js
+++ www/scripts/workout.js
@@ -5,9 +5,12 @@ myApp.controller('WorkoutController', ['$scope', '$rootScope', 'Authentication',
     $scope.currentExercise;
     $scope.exerciseList;
 
+    var exerciseTimeLimit;
+
     $scope.updateExerciseList = function() {
         exerciseList = sharedExercises.getExerciseList();
         $scope.currentExercise = exerciseList[0];
+        exerciseTimeLimit = $scope.currentExercise["time"];
         $scope.exerciseList = exerciseList;
         return exerciseList;
     }
@@ -22,7 +25,47 @@ myApp.controller('WorkoutController', ['$scope', '$rootScope', 'Authentication',
         $scope.exerciseList.splice(toIndex, 0, item)
     }
 
-    
+    $scope.currentExerciseProgress = 0;
+    var timer;
+    var exTime = 0;
+
+    var addTime = function() {
+        exTime+=.1;
+        console.log("exTime", exTime)
+        $scope.$apply(function() {
+            $scope.currentExerciseProgress = exTime;
+        });
+        if (exTime>exerciseTimeLimit) {
+            removeCurrentTask($scope.currentExercise);
+            exTime=0;
+        }
+    }
+
+    var removeCurrentTask = function(task) {
+        $scope.exerciseList.shift();
+
+        console.log($scope.exerciseList);
+
+        // sharedExercises.setExerciseList($scope.getExerciseList);
+
+        exerciseList = $scope.exerciseList;
+        // $scope.updateExerciseList();
+    }
+
+    $scope.startExercise = function() {
+        timer = setInterval(addTime, 100);
+
+        // console.log(exerciseTimeLimit, "exerciseTimeLimit");
+        // if the timer exceeds maxTime, delete the current task, and restart exTime
+
+
+
+
+    }
+
+    $scope.pauseExercise = function() {
+        clearInterval(timer);
+    }
 
     // $scope.testEL = function() {
     //     // $scope.exerciseList = sharedExercises.getExerciseList();
